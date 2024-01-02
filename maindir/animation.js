835e1b40b8e2d2c0c99011ba16834b1c5b9db079
Bryan A <brynwrld999@gmail.com>
2024-01-01 23:46:42 -0500

    configuring for git, deleting some useless files

---
+++ maindir/animation.js
@@ -0,0 +1,30 @@
+// const frames = [
+//   "  ___   __",
+//   " ( o ) ( o )",
+//   "{'.'=   = '.'}",
+//   "    /     \\",
+//   "   / _    _\\",
+//   "  /  |    |  \\",
+//   " (   |    |   )",
+//   "   \\ |    | /",
+//   "    \\|____|/",
+// ];
+
+// const asciiArtElement = document.getElementsByClassName('ascii-art')[0];
+// let currentFrameIndex = 0;
+
+// function updateAsciiArt(){
+//   asciiArtElement.textContent = frames[currentFrameIndex];
+//   currentFrameIndex = (currentFrameIndex +1) % frames.length;
+// }
+// const delay = 500;
+// const animationInterval = setInterval(updateAsciiArt, delay);
+
+// const totalFrames = 30;
+//   setTimeout(() => {
+//     clearInterval(animationInterval);
+//     asciiArtElement.textContent = 'Animation finished!';
+//   }, (totalFrames*delay));
+
+// // Start the animation when the page loads
+// document.addEventListener('DOMContentLoaded', startAnimation);
